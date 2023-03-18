da742a5572dd6364754d68d0b3e16b4821c966da
Abhijeet Mane <126258303+Abhijeetmane34@users.noreply.github.com>
2023-03-18 13:56:59 +0530

    Add files via upload
    Program to print 5 to 1 numbers on screen

---
+++ Program1_3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void Display()
+{
+    int i = 1;
+
+    int iNo = 5;
+    for(iNo=5 ; i==iNo;)
+    {
+        printf(" Abhijeet %d",i);
+        i--;
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
